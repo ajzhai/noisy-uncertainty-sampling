6a8d5f3ba1c773df04e58b979015ad7a55f2e1d4
Albert <tsengalb99@gmail.com>
2020-03-11 23:02:55 -0700

    multithread synthetic.py

--- synthetic.py
+++ synthetic.py
@@ -4,7 +4,8 @@ from modAL.models import ActiveLearner
 from modAL.uncertainty import entropy_sampling
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
-
+from multiprocessing import Pool
+import multiprocessing as mp
 
 def load_data(filename, n_train):
     """Load a dataset CSV, shuffle, and split into train and test."""
@@ -20,44 +21,54 @@ def load_data(filename, n_train):
     y_train, y_test = y[:n_train], y[n_train:]
     return X_train, X_test, y_train, y_test
 
+def run_exp(intup):
+    global X_train, X_test, y_train, y_test
+    rep, i, p = intup
 
-if __name__ == '__main__':
-    np.random.seed(165)
-    X_train, X_test, y_train, y_test = load_data('wdbc_data.csv', 300)
+    # Make noisy data, simulate pool-based case
+    y_train_noisy = utils.flip_labels(y_train, p)
+    X_seed, X_pool = X_train[:n_seed], X_train[n_seed:]
+    y_seed, y_pool = y_train_noisy[:n_seed], y_train_noisy[n_seed:]
 
-    n_seed = 5
-    query_budget = 50
-    reps = 100
-    ps = [0, 0.1, 0.2, 0.3]
-    log_interval = 10
-    all_results = np.zeros((reps, len(ps), query_budget - n_seed))
+    # Initializing the learner
+    learner = ActiveLearner(
+        estimator=RandomForestClassifier(n_estimators=10),
+        query_strategy=entropy_sampling,
+        X_training=X_seed, y_training=y_seed
+    )
 
-    for rep in range(reps):
-        for i, p in enumerate(ps):
-            # Make noisy data, simulate pool-based case
-            y_train_noisy = utils.flip_labels(y_train, p)
-            X_seed, X_pool = X_train[:n_seed], X_train[n_seed:]
-            y_seed, y_pool = y_train_noisy[:n_seed], y_train_noisy[n_seed:]
+    # Run active learning and record history of test accuracy
+    history = np.zeros(query_budget - n_seed)
+    for j in range(query_budget - n_seed):
+        query_idx, query_inst = learner.query(X_pool)
+        learner.teach(X_pool[query_idx], y_pool[query_idx])
+        history[j] = learner.score(X_test, y_test)
+    return history
 
-            # Initializing the learner
-            learner = ActiveLearner(
-                estimator=RandomForestClassifier(n_estimators=10),
-                query_strategy=entropy_sampling,
-                X_training=X_seed, y_training=y_seed
-            )
+np.random.seed(165)
+X_train, X_test, y_train, y_test = load_data('wdbc_data.csv', 300)
 
-            # Run active learning and record history of test accuracy
-            history = np.zeros(query_budget - n_seed)
-            for j in range(query_budget - n_seed):
-                query_idx, query_inst = learner.query(X_pool)
-                learner.teach(X_pool[query_idx], y_pool[query_idx])
-                history[j] = learner.score(X_test, y_test)
-            all_results[rep, i] = history
+n_seed = 5
+query_budget = 50
+reps = 100
+ps = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+log_interval = 10
+all_results = np.zeros((reps, len(ps), query_budget - n_seed))
 
-        if rep % log_interval == log_interval - 1:
-            print('rep', rep + 1)
+rtup = []
+for rep in range(reps):
+    for i, p in enumerate(ps):
+        rtup.append((rep, i, p))
 
-    tags = list(map(lambda p: 'p=' + str(p), ps))
-    results = np.mean(all_results, axis=0)
-    utils.plot_learning_curves(results, range(n_seed + 1, query_budget + 1),
-                               tags, 'synthetic_noisy_labels.png')
+pool = Pool(mp.cpu_count())
+histories = pool.map(run_exp, rtup)
+
+for it in range(len(rtup)):
+    rep, i, p = rtup[it]
+    all_results[rep, i] = histories[it] 
+
+
+tags = list(map(lambda p: 'p=' + str(p), ps))
+results = np.mean(all_results, axis=0)
+utils.plot_learning_curves(results, range(n_seed + 1, query_budget + 1),
+                           tags, 'synthetic_noisy_labels.png')
